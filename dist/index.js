ccbc0dc3250c8a5d96f5a0aa5b55ab7f7a6f8293
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-05-15 14:41:05 +0500

    feat: built-in types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
 let a = 5;
+let b = 'as';
+let c = true;
+let d;
+d = 5;
+d = "asde";
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-let a: number = 5;
+let a = 5; //number
+let b = 'as'; //string
+let c: boolean = true; //boolean
+let d; //any type
+
+d = 5;
+d = "asde"
